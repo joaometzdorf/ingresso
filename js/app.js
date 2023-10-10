115955b723089e0730c10753a2e40707bab32f3b
João Victor Metzdorf <143990988+joaometzdorf@users.noreply.github.com>
2023-10-09 22:18:03 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,47 @@
+function comprar(){
+    let ingresso = document.getElementById('tipo-ingresso');
+    let quantidade = parseInt(document.getElementById('qtd').value);
+    
+    if(ingresso.value == 'pista') {
+        comprarPista(quantidade);
+    }
+    else if(ingresso.value == 'superior') {
+        comprarSuperior(quantidade);
+    }
+    else {
+        comprarInferior(quantidade);
+    }
+}
+
+function comprarPista(quantidade) {
+    let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(quantidade > quantidadePista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        quantidadePista = quantidadePista - quantidade;
+        document.getElementById('qtd-pista').textContent = quantidadePista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(quantidade) {
+    let quantidadeSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(quantidade > quantidadeSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        quantidadeSuperior = quantidadeSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = quantidadeSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(quantidade) {
+    let quantidadeInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(quantidade > quantidadeInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        quantidadeInferior = quantidadeInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = quantidadeInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
